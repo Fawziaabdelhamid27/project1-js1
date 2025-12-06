ffee768659d14dd62e73249a60db7c1e9e1e4a3c
Fawziaabdelhamid27 <154511298+Fawziaabdelhamid27@users.noreply.github.com>
2025-12-06 15:35:15 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
-document.addEventListener('DOMContentLoaded', function ( ) {
-      const recipeSections = document.querySelectorAll('.recipe-section');
-      const tryAnotherButtons = document.querySelectorAll('.try-another-btn');
-      let currentRecipeIndex = 0;
-
-      function showRecipe(index) {
-
-        recipeSections.forEach(section => {
-          section.classList.remove('active');
-        });
-
-        recipeSections[index].classList.add('active');
-      }
-
-      tryAnotherButtons.forEach(button => {
-        button.addEventListener('click', () => {
-
-          currentRecipeIndex = (currentRecipeIndex + 1) % recipeSections.length;
-          showRecipe(currentRecipeIndex);
-        });
-      });
-      showRecipe(currentRecipeIndex);
-    });
+function showRecipe(id){
+  var sections= document.getElementsByClassName('recipe-section')
+  for(var i=0 ;i<sections.length;i++){
+    sections[i].style.display='none'
+  }
+  document.getElementById(id).style.display='block'
+}
